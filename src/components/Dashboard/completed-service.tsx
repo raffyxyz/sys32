9ef6f5d23090da7d6391a04369a2393692d9af26
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-11 17:37:59 +0800

    chore: update dashboard

---
+++ src/components/Dashboard/completed-service.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface CompletedServiceProps {}
+
+export const CompletedService: React.FC<CompletedServiceProps> = () => {
+  return <div>CompletedService</div>;
+};
