ce91e705c91d20cd7a7d75a626e2b8a8752e230c
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-11 17:36:48 +0800

    chore: delete

--- src/app/order/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import DashboardLayout from "@/layouts/dashboard/dashboard-layout";
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
