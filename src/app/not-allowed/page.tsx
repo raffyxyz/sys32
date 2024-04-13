0736fd9fdad6fd8c6254e7256f0d8e5aa3d70202
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-13 22:53:44 +0800

    feat: basic not allowed page

---
+++ src/app/not-allowed/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotAllowedPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-semibold">No permission!</h1>
+    </div>
+  );
+}
